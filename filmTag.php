75681e5c52a1f6a991512d740d84f56cb8d872b4
Rob <rob@mintsoft.net>
2012-07-15 13:35:29 +0100

    Adding warning for more than 1 page of returned results

--- filmTag.php
+++ filmTag.php
@@ -8,23 +8,24 @@ $tmdb_V3->setLang("en");
 
 $searchOption="";
 $obj="";
-$opts = getopt("i:n:",array());
+$opts = getopt("i:n:l",array());
 
 if(isset($opts['n']))
 {
 	$obj = $tmdb_V3->searchMovie($opts['n']);
-
 	echo str_pad("tmdb_id",8," ",STR_PAD_LEFT)." | ".str_pad("Release Date",12," ",STR_PAD_LEFT)." | Title\n";
-
 	foreach($obj['results'] as $searchResult)
 	{
 		echo str_pad($searchResult['id'],8," ",STR_PAD_LEFT)." | ".str_pad($searchResult['release_date'],12," ",STR_PAD_LEFT)." | {$searchResult['title']}\n";
 	}
+
+	if($obj['total_pages']>1)
+		echo "# Multiple Pages of results ({$obj['total_pages']} returned - only the first is displayed\n";
 }
 elseif(isset($opts['i']))
 {
 	$obj = $tmdb_V3->movieDetail($opts['i']);
-	
+
 	$output = $outputTemplate;
 	$output = str_replace( "%TMDB_ID%",			htmlentities($obj['id']), $output);
 	$output = str_replace( "%IMDB_ID%",			htmlentities($obj['imdb_id']), $output);
@@ -33,11 +34,17 @@ elseif(isset($opts['i']))
 	$output = str_replace( "%ORIGINAL_TITLE%",	htmlentities($obj['original_title']), $output);
 
 	print $output;
+	//-l means don't include the extra \n on the end of the file
+	if(!isset($opts['l']))
+	{
+		echo "\n";
+	}
 }
 else
 {
-	echo "Specify -i for id or -n for name\n";
+	echo "Specify -i for id or -n for search by name\n";
 	exit(1);
 }
+
 exit(0);
 ?>
